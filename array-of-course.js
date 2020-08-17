7904b8ff900ca337e00b157f5e40b122702273e4
Angelito <angel.vasquez.unam@gmail.com>
2020-08-16 21:46:55 -0500

    Add list of courses

---
+++ array-of-course.js
@@ -0,0 +1,29 @@
+const courses = [
+  {
+    name: 'Design Thinking',
+    image: 'put some image',
+    lessons: 15,
+  },
+  {
+    name: 'UI design',
+    image: 'put some image',
+    lessons: 30,
+  },
+  {
+    name: 'UX design',
+    image: 'put some image',
+    lessons: 22,
+  },
+  {
+    name: 'Digital Design',
+    image: 'put some image',
+    lessons: 25,
+  },
+  {
+    name: 'UI Interactions',
+    image: 'put some image',
+    lessons: 15,
+  },
+]
+
+export default courses
